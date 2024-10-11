3ff7dbe697db69d9c8ffa7279c7689d6136c6136
Juan Torres <juan93git@gmail.com>
2024-10-11 12:20:06 +0200

    Updated scripts for docker volume

--- jenkins/build/build.sh
+++ jenkins/build/build.sh
@@ -4,9 +4,11 @@ echo "**************************"
 echo "Building the Maven project"
 echo "**************************"
 
+WORKSPACE=$(HOME)/workspace/pipeline-docker-maven
+
 docker run --rm -w /app \
     --name maven \
     --ulimit nofile=122880:122880 \
-    -v $PWD/simple-java-maven-app:/app \
-    -v $PWD/build-from-container:/root/.m2 \
+    -v $WORKSPACE/simple-java-maven-app:/app \
+    -v $WORKSPACE/build-from-container:/root/.m2 \
     maven:3.9.2 "$@"
